c5ce558028cbd2fe7a8091603aee5a8ddadc0aab
bima rijaluddin <bimarijal@gmail.com>
2019-09-19 11:32:49 +0800

    Tugas 4 Javascript
    Tugas dari niomic

---
+++ tugas4.js
@@ -0,0 +1,47 @@
+var andi = 190;
+var indra = 170;
+var rijal = 180;
+
+if(andi > indra){
+    if (andi > rijal){
+        console.log("andi siswa paling tinggi");
+    }else{
+        console.log("andi siswa paling tinggi kedua");
+    }
+}else{
+    if(andi < rijal){
+        console.log("andi siswa paling pendek");
+    }else{
+        console.log("andi siswa paling pendek kedua");
+    }
+}
+
+
+// if(rijal > indra){
+//     if (rijal > andi){
+//         console.log("rijal siswa paling tinggi");
+//     }else{
+//         console.log("rijal siswa paling tinggi kedua");
+//     }
+// }else{
+//     if(rijal < indra){
+//         console.log("rijal siswa paling pendek");
+//     }else{
+//         console.log("rijal siswa paling pendek kedua");
+//     }
+// }
+
+
+// if(indra > andi){
+//     if (indra > rijal){
+//         console.log("indra siswa paling tinggi");
+//     }else{
+//         console.log("indra siwa paling tinggi kedua");
+//     }
+// }else{
+//     if(indra < rijal){
+//         console.log("indra siswa paling pendek");
+//     }else{
+//         console.log("indra siswa paling pendek kedua");
+//     }
+// }
